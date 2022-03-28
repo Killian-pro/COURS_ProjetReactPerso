1c4a648b5fc2f803e71f857c4aa96a9881ca56c6
Killian <cluzel.killian@gmail.com>
2022-03-28 17:24:29 +0200

    sabre laser

--- src/Componant/Movie.js
+++ src/Componant/Movie.js
@@ -1,14 +1,40 @@
-
+import { useEffect, useState } from 'react';
 function GetFile({ film }) {
+    const [imgFilm, setImgFilm] = useState('');
+    const poster = ['https://upload.wikimedia.org/wikipedia/en/4/40/Star_Wars_Phantom_Menace_poster.jpg', 'https://upload.wikimedia.org/wikipedia/en/3/32/Star_Wars_-_Episode_II_Attack_of_the_Clones_%28movie_poster%29.jpg', 'https://upload.wikimedia.org/wikipedia/en/9/93/Star_Wars_Episode_III_Revenge_of_the_Sith_poster.jpg',
+        'https://upload.wikimedia.org/wikipedia/en/8/87/StarWarsMoviePoster1977.jpg', 'https://upload.wikimedia.org/wikipedia/en/3/3f/The_Empire_Strikes_Back_%281980_film%29.jpg', 'https://upload.wikimedia.org/wikipedia/en/b/b2/ReturnOfTheJediPoster1983.jpg']
+
+    console.log(film)
+
+    useEffect(() => {
+        getImageFilm()
+    }, [])
+
+    function getImageFilm() {
+        fetch(`https://pixabay.com/api/?key=26359715-4c1aa25d34cc2168dca3b4a1b&q=${film?.title}&image_type=photo`)
+            .then(res => res.json())
+            .then(data => { setImgFilm(data.hits[0]?.previewURL) });
+    }
 
 
     return (
-        <div style={{ color: 'white' }} >
-            <div >
-                {film?.episode_id}
-            </div>
-            <div >
-                {film?.title} - {film?.release_date}
+        <div style={{ color: 'white', padding: 15 }} >
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div style={{ width: 300 }}>
+                    <img src={poster[film?.episode_id - 1]} />
+                </div>
+
+                <div>
+                    <div style={{ fontSize: 24 }} >
+                        EPISODE {film?.episode_id} :
+                    </div>
+                    <div >
+                        {film?.title} - {film?.release_date}
+                    </div>
+                    <div >
+                        created by : {film?.director} & {film?.producer}
+                    </div>
+                </div>
             </div>
 
         </div>
--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -3,10 +3,22 @@ import Movie from '../Componant/Movie';
 
 function Home() {
     const [movies, setMovies] = useState([]);
+    const [scrollTop, setScrollTop] = useState(0);
+    const [scrolling, setScrolling] = useState(false);
 
     useEffect(() => {
         appelFilm()
     }, [])
+
+    useEffect(() => {
+        const onScroll = e => {
+            setScrollTop(e.target.documentElement.scrollTop);
+            setScrolling(e.target.documentElement.scrollTop > scrollTop);
+        };
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [scrollTop]);
 
     function appelFilm() {
         fetch('https://swapi.dev/api/films')
@@ -19,12 +31,26 @@ function Home() {
     }
 
     return (
-        <div style={{ height: '100vh', width: '100vw', backgroundColor: 'black' }}>
+        <div style={{ backgroundColor: 'black', padding: '2vw', }}>
             {movies?.length > 1 &&
                 movies.map((it, index) =>
                     <div key={index}>
                         <Movie film={it} />
                     </div>)}
+            <div style={{ position: 'fixed', left: '80%', top: '10%', color: 'white', fontSize: 25, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <div style={{ border: 'solid 2px black', height: 500, width: 25, display: 'flex', flexDirection: 'column-reverse' }}>
+                    <div style={{ backgroundColor: 'red', height: scrollTop / 4, transition: ' height 2s', borderTopLeftRadius: 15, borderTopRightRadius: 15 }}></div>
+                </div>
+                <div style={{ border: 'solid 2px white', height: 20, width: 100, backgroundColor: 'white' }}>
+
+                </div>
+                <div style={{ border: 'solid 2px white', height: 50, width: 25, backgroundColor: 'white' }}>
+
+                </div>
+                <div style={{ border: 'solid 2px red', height: 10, width: 25, backgroundColor: 'red' }}>
+
+                </div>
+            </div>
         </div>
     );
 }
